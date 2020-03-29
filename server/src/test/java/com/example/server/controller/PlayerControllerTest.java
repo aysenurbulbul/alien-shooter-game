354080362d01535bb5c479f1b71cb81f750c0af9
aysenur.bulbul <aysenur.bulbul@metu.edu.tr>
2020-03-29 18:04:04 +0300

    Added new test cases to PlayerController

--- server/src/test/java/com/example/server/controller/PlayerControllerTest.java
+++ server/src/test/java/com/example/server/controller/PlayerControllerTest.java
@@ -12,16 +12,16 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import java.util.Optional;
 
-import static org.junit.Assert.*;
+
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
@@ -38,7 +38,7 @@ public class PlayerControllerTest {
     PlayerController playerController;
 
     @Autowired
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @Before
     public void setUp(){
@@ -57,5 +57,36 @@ public class PlayerControllerTest {
             throw new RuntimeException(e);
         }
     }
-    
+
+    @Test
+    public void getPlayer() throws Exception{
+        Player player = new Player(1L,"username1", "email@mail.com", "password");
+
+        when(playerService.getPlayer("username1")).thenReturn(player);
+
+        mockMvc.perform(get("/players/{username}", "username1")).andExpect(status().isOk());
+        then(playerService).should().getPlayer("username1");
+    }
+
+    @Test
+    public void registerPlayer() throws Exception{
+        Player player = new Player(1L,"username", "email@mail.com", "password");
+        when(playerService.addPlayer(any(Player.class))).thenReturn(player);
+
+        mockMvc.perform(post("/registration").content(asJsonString(player))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+
+        then(playerService).should().addPlayer(any(Player.class));
+    }
+
+    @Test
+    public void deletePlayer() throws Exception{
+        Player player = new Player(1L,"username1", "email@mail.com", "password");
+        when(playerRepository.findById(anyLong())).thenReturn(Optional.of(player));
+
+        mockMvc.perform(delete("/delete/{playerId}",1L)).andExpect(status().isOk());
+        then(playerService).should().deletePlayer(anyLong());
+    }
+
 }
