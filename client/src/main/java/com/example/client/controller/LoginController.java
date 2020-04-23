22a68a29165803cc10e208f664e0d8526a95b6aa
aysenur.bulbul <aysenur.bulbul@metu.edu.tr>
2020-04-23 23:05:19 +0300

    Login function added

--- client/src/main/java/com/example/client/controller/LoginController.java
+++ client/src/main/java/com/example/client/controller/LoginController.java
@@ -3,34 +3,84 @@ package com.example.client.controller;
 import com.example.client.StageInitializer;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 @Component
-public class LoginController {
+public class LoginController implements Initializable {
+
+    private RestTemplate restTemplate;
 
     @FXML
-    public Button backMenu;
+    public Button backMenuButton;
 
     @FXML
-    public TextField username;
+    public TextField usernameField;
 
     @FXML
-    public PasswordField password;
+    public PasswordField passwordField;
 
     @FXML
-    public Button login;
+    public Button loginButton;
 
     @FXML
     private void loadMainMenu() throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("/fxml/MainMenu.fxml"));
         Stage mainStage = StageInitializer.parentStage;
         mainStage.getScene().setRoot(parent);
+    }
+
+    @FXML
+    private void login(){
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        String jsonString = new JSONObject()
+                .put("username", username)
+                .put("password", password).toString();
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> httpEntity = new HttpEntity<>(jsonString, httpHeaders);
+
+        try {
+            restTemplate.exchange("http://localhost:8080/login",
+                    HttpMethod.POST,
+                    httpEntity,
+                    String.class);
+            messageAlert(Alert.AlertType.CONFIRMATION,"Successfully logged in!", "Hello " + username + "!");
+        } catch (RestClientException e){
+            messageAlert(Alert.AlertType.ERROR, "Error", "Wrong credentials");
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        restTemplate = new RestTemplate();
+    }
+
+    private void messageAlert(Alert.AlertType alertType, String title, String message){
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
     }
 }
