c47488a79922fee8c59f484ecc978658a41bdf77
noxlumos <97zeyneperdogan@gmail.com>
2020-04-24 03:03:09 +0300

    Game and Player model added

---
+++ client/src/main/java/com/example/client/model/Game.java
@@ -0,0 +1,10 @@
+package com.example.client.model;
+
+import java.time.LocalDateTime;
+
+public class Game {
+    private Long id;
+    private Player player;
+    private Long score;
+    private LocalDateTime finishDateTime;
+}
---
+++ client/src/main/java/com/example/client/model/Player.java
@@ -0,0 +1,8 @@
+package com.example.client.model;
+
+public class Player {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+}
