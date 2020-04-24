8990d663cb661eb260c7628a358761829e583de1
aysenur.bulbul <aysenur.bulbul@metu.edu.tr>
2020-04-24 21:53:27 +0300

    Player ship added

--- client/src/main/java/com/example/client/view/GameView.java
+++ client/src/main/java/com/example/client/view/GameView.java
@@ -1,8 +1,10 @@
 package com.example.client.view;
 
 import com.example.client.StageInitializer;
+import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -11,6 +13,7 @@ public class GameView {
     private AnchorPane anchorPane;
     private Scene gameScene;
     private Stage gameStage;
+    private final String playerShipPath = "/static/playerShip1_red.png";
 
     public GameView(){
         anchorPane = new AnchorPane();
@@ -18,7 +21,10 @@ public class GameView {
         gameStage = new Stage();
     }
 
-    public void gameStart(){
+    public void gameStart() {
+        Image cursorImage = new Image(playerShipPath);
+        gameScene.setCursor(new ImageCursor(cursorImage));
+
         gameStage = StageInitializer.parentStage;
         gameStage.setScene(gameScene);
         Button button = new Button();
