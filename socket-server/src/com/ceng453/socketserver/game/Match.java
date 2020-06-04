fbb9585435731eea1e19bb308171439b82c86695
aysenur.bulbul <aysenur.bulbul@metu.edu.tr>
2020-06-04 18:47:20 +0300

    alien random move and shoot server side

--- socket-server/src/com/ceng453/socketserver/game/Match.java
+++ socket-server/src/com/ceng453/socketserver/game/Match.java
@@ -13,6 +13,8 @@ public class Match implements Runnable {
     private boolean gameOn = true;
     private Double[] gamer1Coords;
     private Double[] gamer2Coords;
+    private Double alienX = 300.0;
+    private Double alienY = 150.0;
     private Double[] alienCoords;
 
     public Match(Gamer gamer1, Gamer gamer2){
@@ -25,6 +27,8 @@ public class Match implements Runnable {
         gamer1Coords = new Double[2];
         gamer2Coords = new Double[2];
         alienCoords = new Double[2];
+        alienCoords[0] = alienX;
+        alienCoords[1] = alienY;
         try {
             sendUsername(gamer1, gamer2);
             sendUsername(gamer2, gamer1);
@@ -36,19 +40,22 @@ public class Match implements Runnable {
         }
         while (gameOn){
             try {
-                //update alien health
-                sendHealth(gamer1, gamer2);
-                sendHealth(gamer2, gamer1);
-                //move alien
-                sendData(gamer1, gamer2, alienCoords);
-                //final alien shoot
-                sendAlienShootRandom(gamer1,gamer2);
-                //move enemy ship
+                // send each other their coordinate data
                 sendData(gamer1, gamer2, gamer1Coords);
                 sendData(gamer2, gamer1, gamer2Coords);
+                // send random alien coordinates
+                sendAlienCoords(gamer1, gamer2);
+                //update alien health
+                //sendHealth(gamer1, gamer2);
+                //sendHealth(gamer2, gamer1);
+                //move alien
+                //sendData(gamer1, gamer2, alienCoords);
+                //final alien shoot
+                //sendAlienShootRandom(gamer1,gamer2);
+                //move enemy ship
                 //update enemy player health
-                sendHealth(gamer1, gamer2);
-                sendHealth(gamer2, gamer1);
+                //sendHealth(gamer1, gamer2);
+                //sendHealth(gamer2, gamer1);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -66,21 +73,28 @@ public class Match implements Runnable {
         }
     }
 
-    public void sendUsername(Gamer gamer1, Gamer gamer2) throws IOException {
+    private boolean moveAlien(Gamer gamer) throws IOException {
+        in = new DataInputStream(gamer.getSocket().getInputStream());
+        boolean status = in.readBoolean();
+        return status;
+    }
+
+
+    private void sendUsername(Gamer gamer1, Gamer gamer2) throws IOException {
         in = new DataInputStream(gamer1.getSocket().getInputStream());
         String gamer1Username = in.readUTF();
         out = new DataOutputStream(gamer2.getSocket().getOutputStream());
         out.writeUTF(gamer1Username);
     }
 
-    public void sendHealth(Gamer gamer1, Gamer gamer2) throws IOException {
+    private void sendHealth(Gamer gamer1, Gamer gamer2) throws IOException {
         in = new DataInputStream(gamer1.getSocket().getInputStream());
         int gamer1Health = in.readInt();
         out = new DataOutputStream(gamer2.getSocket().getOutputStream());
         out.writeInt(gamer1Health);
     }
 
-    public void setGameControllerPlayer(Gamer gamer1, Gamer gamer2) throws IOException{
+    private void setGameControllerPlayer(Gamer gamer1, Gamer gamer2) throws IOException{
         out = new DataOutputStream(gamer1.getSocket().getOutputStream());
         out.writeInt(1);
         out = new DataOutputStream(gamer2.getSocket().getOutputStream());
@@ -92,4 +106,31 @@ public class Match implements Runnable {
         out = new DataOutputStream(gamer2.getSocket().getOutputStream());
         out.writeDouble(alienShootRandom);
     }
+
+    private void alienCoordinates() {
+        Double randomX = -50 + (Math.random() * ((50 - (-50)) + 1));
+        Double randomY = -50 + (Math.random() * ((50 - (-50)) + 1));
+        if(50 < alienX + randomX && alienX + randomX < 650)
+            alienX += randomX;
+        if(50 < alienY + randomY && alienY + randomY < 300)
+            alienY += randomY;
+        alienCoords[0] = alienX;
+        alienCoords[1] = alienY;
+    }
+
+    private void sendAlienCoords(Gamer gamer1, Gamer gamer2) throws IOException {
+        boolean gamer1Move = moveAlien(gamer1);
+        boolean gamer2Move = moveAlien(gamer2);
+        if(gamer1Move || gamer2Move)
+            alienCoordinates();
+        out = new DataOutputStream(gamer1.getSocket().getOutputStream());
+        for(int i=0; i<2; i++){
+            out.writeDouble(alienCoords[i]);
+        }
+        out = new DataOutputStream(gamer2.getSocket().getOutputStream());
+        for(int i=0; i<2; i++){
+            out.writeDouble(alienCoords[i]);
+        }
+    }
+
 }
