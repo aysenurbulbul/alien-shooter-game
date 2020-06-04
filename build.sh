07e200997d8da7b8bcd06bee87871adceecef042
zeynep.erdogan <97zeyneperdogan@gmail.com>
2020-06-04 22:04:15 +0300

    build.sh socket-server jar added

--- build.sh
+++ build.sh
@@ -7,3 +7,10 @@ cd client
 ./mvnw clean package
 cd ..
 mv client/target/client_program21.jar executables/
+cd socket-server
+javac -d . src/com/ceng453/socketserver/game/*.java src/com/ceng453/socketserver/server/*.java src/com/ceng453/socketserver/*.java
+jar cvf socket_server_program21.jar com/ceng453/socketserver/*.class com/ceng453/socketserver/server/*.class com/ceng453/socketserver/game/*.class
+echo "Main-Class: com.ceng453.socketserver.Main" > MANIFEST.MF
+jar cvmf MANIFEST.MF socket_server_program21.jar com/ceng453/socketserver/*.class com/ceng453/socketserver/server/*.class com/ceng453/socketserver/game/*.class
+cd ..
+mv socket-server/socket_server_program21.jar executables/
